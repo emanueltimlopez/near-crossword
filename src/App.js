7a20c98d6b919c31dc80af0002ffd469e46ea401
Ema López <emalopez@MacBook-Pro-de-Ema.local>
2021-11-29 09:54:56 -0300

    fix hook

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ const App = ({ data, solutionHash }) => {
         await checkSolution(gridData);
       }
     },
-    []
+    [checkSolution, data]
   );
 
   async function checkSolution(gridData) {
